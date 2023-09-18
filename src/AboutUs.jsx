3cca38f0bd45e293758a91c90c126fa11f75f648
Sergi-oh <sehernan96@gmail.com>
2023-09-17 23:25:23 -0700

    about

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,7 +1,15 @@
+import {Route, Routes, Link} from 'react-router-dom'
+
 const AboutUs = ()=> {
   return (
-    <div>
-      TODO: Add some information about Us!
+    <div className='about'>
+      <h1>All About Us</h1>
+      <p>This is a place where you can buy and sell!</p>
+      <p>Browse our listings or create one yourself!</p>
+      <Link to='/contact'>Reach out to us!</Link>
+      <Routes>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
     </div>
   );
 };
